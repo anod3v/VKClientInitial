a9fcb75cee6696de5cca23752ece785548c23a60
anod3v <anod3v@gmail.com>
2020-12-23 21:00:15 +0300

    minor adjustments

---
+++ homeWork_1/controllers/news/VKNewsFeedViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  VKFeedViewModel.swift
+//  homeWork_1
+//
+//  Created by Andrey on 23/12/2020.
+//  Copyright © 2020 Марат Нургалиев. All rights reserved.
+//
+
+import UIKit
+
+struct VkFeedViewModel {
+    let feedSourceName: String?
+    let feedDate: String?
+    let feedText: String?
+    let feedSourceImageView: UIImageView
+    let feedItemImageView: UIImageView
+    let likesCount: String?
+    let viewsCount: String?
+    let repostsCount: String?
+    let commentsCount: String?
+    let viewRatio: CGFloat
+}
